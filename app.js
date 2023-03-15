5aa175184519ad3ea5e5752e954ed64328fffec5
vishalvenkat12 <vishalvenkat1206@gmail.com>
2023-03-15 15:16:22 +0530

    Showing form data in console

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-app.use((req,res,next)=>{
-    console.log('In a middleware');
-    next();
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<html><body><form action="/product" method="POST"><label for="name">Name</label><input type="text" name="title" id="name"><label for="size">Size</label><input type="number" name="size" id="size"><button type="submit">Add Product</button></form></body></html>');
 });
-app.use((req,res,next)=>{
-    console.log('In another middleware');
-    res.send('<h1>Welcome to Express!</h1>');
+
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/',(req,res,next)=>{
+    res.send('<h1>Welcome to express</h1>');
 });
 app.listen(3000);
