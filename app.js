a8d701d286d6b104349121c751778f761a210414
vishalvenkat12 <vishalvenkat1206@gmail.com>
2023-03-15 15:54:21 +0530

    Code clean up and adding routes

--- app.js
+++ app.js
@@ -2,18 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes= require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/add-product',(req,res,next)=>{
-    res.send('<html><body><form action="/product" method="POST"><label for="name">Name</label><input type="text" name="title" id="name"><label for="size">Size</label><input type="number" name="size" id="size"><button type="submit">Add Product</button></form></body></html>');
+app.use('/admin', adminRoutes);
+app.use('/shop', shopRoutes);
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found!</h1>');
 });
 
-app.use('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/',(req,res,next)=>{
-    res.send('<h1>Welcome to express</h1>');
-});
 app.listen(3000);
